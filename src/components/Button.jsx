17c86a3d550428fbb1032d661e1f5133d3312482
Paulo Teixeira <hst.paulo@gmail.com>
2021-01-21 22:27:57 -0300

    adicionando funcionalidade que recebe os valores do botao ao clicar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './Button.css'
 
 export default function Button(props){
+
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
     return(
-        <button className="button">{props.label}</button>
+        <button 
+            onClick={evento => props.click && props.click(evento.target.innerHTML)}//ou evento.target.innerHTML
+            className={classes}
+        >
+            {props.label}
+        </button>
     )
 }
